b116b892566ab32216114c984b6189aa486d7444
DianaMatei29 <diana.matei@student.unitbv.ro>
2022-11-10 22:42:27 +0200

    Am creat clasa IntrebariGrila impreuna cu constructorul acesteia!

---
+++ Triviador/IntrebariGrila.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+#include<array>
+class IntrebariGrila
+{public:
+	std::string enunt;
+	std::array < std::string, 4> raspunsuri;
+	int index_rasp_corect;
+
+public:
+	IntrebariGrila() = default;
+	IntrebariGrila(std::string enunt, std::array < std::string, 4> raspunsuri, int index_rasp_corect);
+	friend std::istream& operator>>(std::istream& in,IntrebariGrila &intrebari );
+	friend std::ostream& operator << (std::ostream & out, const IntrebariGrila& intrebari);
+	~IntrebariGrila();
+};
+
