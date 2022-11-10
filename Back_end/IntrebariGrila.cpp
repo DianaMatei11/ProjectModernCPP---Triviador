45b4d8062c038e41fee68cbfc88fea1d110a5ecc
DianaMatei29 <diana.matei@student.unitbv.ro>
2022-11-10 23:39:52 +0200

    Am supraincarcat operatorii << si >> pentru afisarea intrebarilor!

--- Back_end/IntrebariGrila.cpp
+++ Back_end/IntrebariGrila.cpp
@@ -1,9 +1,35 @@
-#include "IntrebariGrila.h"
+#include "IntrebariGrila.h"#include "IntrebariGrila.h"
 
 IntrebariGrila::IntrebariGrila(std::string enunt, std::array < std::string, 4> raspunsuri, int index_rasp_corect)
 {
 	this->enunt = enunt;
-	this->raspunsuri=raspunsuri;
-	this->index_rasp_corect=index_rasp_corect;
+	this->raspunsuri = raspunsuri;
+	this->index_rasp_corect = index_rasp_corect;
 
+}
+
+std::istream& operator>>(std::istream& IntrebariCuVariante, IntrebariGrila& intrebari)
+{
+	getline(IntrebariCuVariante, intrebari.enunt);
+	std::string str;
+
+	for (int i = 0; i < 4; i++)
+	{
+		getline(IntrebariCuVariante, str);
+		intrebari.raspunsuri[i] = str;
+	}
+	IntrebariCuVariante >> intrebari.index_rasp_corect;
+	return IntrebariCuVariante;
+}
+
+std::ostream& operator <<(std::ostream& out,const IntrebariGrila& intrebare)
+{
+	out << intrebare.enunt;
+	int i = 0;
+	out << "Alegeti raspunsul corect: ";
+	out << "A." << intrebare.raspunsuri[i++] << '\n';
+	out << "B." << intrebare.raspunsuri[i++] << '\n';
+	out << "C." << intrebare.raspunsuri[i++] << '\n';
+	out << "D." << intrebare.raspunsuri[i] << '\n';
+	return out;
 }
--- Back_end/IntrebariGrila.h
+++ Back_end/IntrebariGrila.h
@@ -1,18 +1,18 @@
 #pragma once
 #include<iostream>
 #include<fstream>
+#include<string>
 #include<array>
 class IntrebariGrila
 {public:
 	std::string enunt;
-	std::array < std::string, 4> raspunsuri;
+	std::array <std::string,4> raspunsuri;
 	int index_rasp_corect;
-
 public:
 	IntrebariGrila() = default;
 	IntrebariGrila(std::string enunt, std::array < std::string, 4> raspunsuri, int index_rasp_corect);
-	friend std::istream& operator>>(std::istream& in,IntrebariGrila &intrebari );
-	friend std::ostream& operator << (std::ostream & out, const IntrebariGrila& intrebari);
-	~IntrebariGrila();
+	friend std::istream& operator>>(std::istream& IntrebariCuVariante, IntrebariGrila& intrebari);
+	friend std::ostream& operator << (std::ostream& out, const IntrebariGrila& intrebari);
+	//~IntrebariGrila();
 };
 
