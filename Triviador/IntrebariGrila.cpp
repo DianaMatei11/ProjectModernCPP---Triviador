3a332a4762881cce2c8146107f5b7e9590c87b63
DianaMatei29 <diana.matei@student.unitbv.ro>
2022-11-10 22:40:49 +0200

    Am creat lasa IntrebariGrila impreuna cu constructorul acesteia !

--- Triviador/IntrebariGrila.cpp
+++
@@ -1,9 +0,0 @@
-#include "IntrebariGrila.h"
-
-IntrebariGrila::IntrebariGrila(std::string enunt, std::array < std::string, 4> raspunsuri, int index_rasp_corect)
-{
-	this->enunt = enunt;
-	this->raspunsuri=raspunsuri;
-	this->index_rasp_corect=index_rasp_corect;
-
-}
