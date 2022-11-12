fa565d2283c19038126a599e57549d89d256c09d
Cristi <stroescucristian478@yahoo.com>
2022-11-12 20:38:06 +0200

    Am pus conditie pentru parola la inregistrare, am adaugat optiune de "uitare a parolei" cand vrei sa te conectezi, si am refacut switch-case-ul.

--- Back_end/Conect.cpp
+++ Back_end/Conect.cpp
@@ -34,7 +34,7 @@ citireUsername:
 	bool usernameValid = regex_match(name0, usernameRule);
 	if (usernameValid)
 	{
-		std::cout << "[INFO] Username valid!\n";
+		std::cout << "[INFO] Username-ul respecta regulile de validare!\n";
 	}
 	else {
 		std::cout << "[WARNING] Username-ul trebuie sa contina litere mari, litere mici, cifre, sau '_', si sa aibe intre 5 si 15 caractere!\n\n";
@@ -42,44 +42,85 @@ citireUsername:
 	}
 	while (findName(name0) == true)
 	{
-		std::cout << "This name already exists.Please enter another name: ";
+		std::cout << "This name already exists. Please enter another name.\n ";
 		goto citireUsername;
+	}
+	if (!findName(name0))
+	{
+		std::cout << "[INFO] Username-ul este disponibil.\n";
 	}
 	std::cout << "Enter your password: ";
 	std::cin >> pass0;
 	User x(name0, pass0);
+	while (x.checkStrongPassword() != "Strong\n")
+	{
+		std::cout<<x.checkStrongPassword();
+		std::cout << "[WARNING] Introduceti o parola care sa respecte regulile: ";
+		std::string parolaNoua;
+		std::cin >> parolaNoua;
+		x.changePassword(parolaNoua);
+	}
+	std::cout << "[INFO] Username-ul si parola au fost salvate!\n";
 	users.push_back(x);
 }
 void Conect::logIn()
 {
-
 	std::string name0, pass0;
+	//std::vector<User> users=getUsers();
 	std::cout << "Enter your name: ";
 	std::cin >> name0;
-	if (!findName(name0))std::cout << "Your account does not exist";
+	if (!findName(name0))
+		std::cout << "Your account does not exist\n";
 	else {
 		std::cout << "Enter your password: ";
 		std::cin >> pass0;
 		while (!findPass(pass0))
 		{
-			std::cout << "Your password is not correct. Please enter again:";
-			std::cin >> pass0;
+			std::cout << "Your password is not correct.\n";
+			char parolaUitata;
+			std::cout << "Ati uitat parola?(y/n) ";
+			std::cin >> parolaUitata;
+			if (parolaUitata == 'y')
+			{
+				for (auto& user : users)
+				{
+					if (user.getUserName() == name0) {
+						std::string parolaNoua;
+						std::cout << "Parola noua: ";
+						std::cin >> parolaNoua;
+						user.changePassword(parolaNoua);
+						std::cout << "[INFO] Parola a fost schimbata!\n";
+						break;
+					}
+				}
+				std::cout << "Please enter again: ";
+				std::cin >> pass0;
+			}
+			else {
+				std::cout << "Please enter again: ";
+				std::cin >> pass0;
+			}
 		}
 	}
-
+	std::cout << "[INFO] Sunteti conectat!\n";
 }
 
 void Conect::meniu()
 {
-	std::cout << "Your options are:\n1 for sign in\n2 for login\n0 for exit" << std::endl;
 	int x;
-	std::cin >> x;
+	do {
+		std::cout << "Your options are:\n1 for sign in\n2 for login\n0 for exit" << std::endl;
+		std::cout << "Alege o optiune din lista: "; std::cin >> x;
 		switch (x)
 		{
 		case 1: signIn();
 			break;
 		case 2: logIn();
 			break;
-		default:std::cout << "Your option is invalid!";
-	    }
+		case 0:
+			std::cout << "[INFO] Ati iesit din program!\n";
+			break;
+		default:std::cout << "Your option is invalid!\n";
+		}
+	} while (x != 0);
 }
