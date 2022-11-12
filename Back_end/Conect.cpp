ac82782a499134de3f9daa3cb528602b46afab98
iosifGacea <iosifgacea0@gmail.com>
2022-11-12 10:59:49 +0200

    Am facut o funtie pentru creaarea de cont

---
+++ Back_end/Conect.cpp
@@ -0,0 +1,41 @@
+#include "Conect.h"
+#include <iostream>
+std::vector<User> Conect::getUsers()
+{
+	return users;
+}
+
+bool Conect::findName(std::string name)
+{
+	for (int i = 0; i < users.size(); i++)
+	{
+		if (users[i].getUserName() == name)return true;
+	}
+	return false;
+}
+
+bool Conect::findPass(std::string pass)
+{
+	for (int i = 0; i < users.size(); i++)
+	{
+		if (users[i].getPassword() == pass)return true;
+	}
+	return false;
+}
+
+void Conect::signIn()
+{
+	std::string name0, pass0;
+	std::cout << "Enter your name: ";
+	std::cin >> name0;
+	while (findName(name0) == true)
+	{
+		std::cout << "This name already exists.Please enter another name: ";
+		std::cin >> name0;
+	}
+	std::cout << "Enter your password: ";
+	std::cin >> pass0;
+	User x(name0, pass0);
+	users.push_back(x);
+}
+
---
+++ Back_end/Conect.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "User.h"
+#include <vector>
+class Conect
+{
+	std::vector<User> users;
+public:
+	std::vector<User> getUsers();
+	bool findName(std::string name);
+	bool findPass(std::string pass);
+	void signIn();
+	//void logIn();
+	//void meniu();
+};
+
