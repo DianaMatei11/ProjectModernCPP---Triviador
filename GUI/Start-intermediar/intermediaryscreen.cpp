4e6a475a11c1b1aed8bab4e7bd4eed075a0bbdad
SandorViviana <viviana.sandor@student.unitbv.ro>
2022-11-27 00:51:55 +0200

    Introducerea imaginilor utilizate in folder de resurse; actualizare cale

--- GUI/Start-intermediar/intermediaryscreen.cpp
+++ GUI/Start-intermediar/intermediaryscreen.cpp
@@ -6,14 +6,13 @@ IntermediaryScreen::IntermediaryScreen(QWidget *parent) :
     ui(new Ui::IntermediaryScreen)
 {
     ui->setupUi(this);
-    QPixmap bkgnd("E:/Facultate/Anul II/Semestrul I/Modern C++/MainScreen Triviador.jpg");
+    QPixmap bkgnd(":/images/images/MainScreen Triviador.jpg");
     bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
     QPalette palette;
-    //palette.setBrush(QPalette::Background, bkgnd);
     palette.setBrush(QPalette::Window,bkgnd);
     this->setPalette(palette);
 
-    QPixmap pixmap("E:/Facultate/Anul II/Semestrul I/Modern C++/logoFinal.png");
+    QPixmap pixmap(":/images/images/logoFinal.png");
     pixmap= pixmap.scaled(this->size(), Qt::IgnoreAspectRatio);
     ui->logo->setPixmap(pixmap);
 }
--- GUI/Start-intermediar/startscreen.cpp
+++ GUI/Start-intermediar/startscreen.cpp
@@ -7,15 +7,13 @@ StartScreen::StartScreen(QWidget *parent)
     , ui(new Ui::StartScreen)
 {
     ui->setupUi(this);
-    //this->centralWidget()->setStyleSheet("background-image:url(\"E:/Facultate/Anul II/Semestrul I/Modern C++/MainScreen Triviador.jpg""); background-position: center;");
-   QPixmap bkgnd("E:/Facultate/Anul II/Semestrul I/Modern C++/MainScreen Triviador.jpg");
+   QPixmap bkgnd(":/images/images/MainScreen Triviador.jpg");
    bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
    QPalette palette;
-   //palette.setBrush(QPalette::Background, bkgnd);
    palette.setBrush(QPalette::Window,bkgnd);
    this->setPalette(palette);
 
-   QPixmap pixmap("E:/Facultate/Anul II/Semestrul I/Modern C++/logoFinal.png");
+   QPixmap pixmap(":/images/images/logoFinal.png");
    pixmap= pixmap.scaled(this->size(), Qt::IgnoreAspectRatio);
    ui->logo->setPixmap(pixmap);
   // scene->addItem(1, &intscr);
