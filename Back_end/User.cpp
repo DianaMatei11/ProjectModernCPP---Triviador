24de470fc42d1aad5b0a371ec23b34b26111f7dd
iosifGacea <iosifgacea0@gmail.com>
2022-12-04 11:00:18 +0200

    Am scris set-ari pentru clasa User

--- Back_end/User.cpp
+++ Back_end/User.cpp
@@ -1,24 +1,8 @@
 #include "User.h"
 
 User::User(std::string userName, std::string pass) :
-	user_name{userName}, password{pass}
-{
-	m_raspunsIntrebariCorecte = 0;
-	m_raspunsuriTotale = 0;
-	m_meciuriJucate = 0;
-	m_scorMinim = 0;
-	m_scorMaxim = 0;
-}
-
-User::User(std::string userName, std::string pass, int raspunsIntrebariCorecte, int raspunsuriTotale, int meciuriJucate, int scorMaxim, int scorMinim):
 	user_name{ userName }, password{ pass }
-{
-	m_raspunsIntrebariCorecte = raspunsIntrebariCorecte;
-	m_raspunsuriTotale = raspunsuriTotale;
-	m_meciuriJucate = meciuriJucate;
-	m_scorMinim = scorMaxim;
-	m_scorMaxim = scorMinim;
-}
+{}
 
 std::string User::getUserName() const
 {
@@ -28,6 +12,16 @@ std::string User::getUserName() const
 std::string User::getPassword() const
 {
 	return password;
+}
+
+void User::setUserName(std::string a)
+{
+	user_name = a;
+}
+
+void User::setPassword(std::string a)
+{
+	password = a;
 }
 
 void User::changePassword(const std::string& pass)
@@ -109,90 +103,15 @@ again:
 	}
 }
 
-float User::getProcentajRaspunsuriCorecte()
-{
-	if (m_raspunsuriTotale != 0) {
-		return float(m_raspunsIntrebariCorecte) / float(m_raspunsuriTotale);
-	}
-	else
-	{
-		std::cerr << "[WARNING] Nu a fost jucat nici un meci!\n";
-		exit(0);
-	}
-}
-
-int User::getMeciuriJucate()
-{
-	return m_meciuriJucate;
-}
-
-int User::getScorMinim()
-{
-	return m_scorMinim;
-}
-
-int User::getScorMaxim()
-{
-	return m_scorMaxim;
-}
-
-int User::getRaspunsuriCorecte()
-{
-	return m_raspunsIntrebariCorecte;
-}
-
-int User::getRaspunsuriTotale()
-{
-	return m_raspunsuriTotale;
-}
-
-void User::updateRaspunsuriCorecte()
-{
-	m_raspunsIntrebariCorecte++;
-}
-
-void User::updateRaspunsuriTotale()
-{
-	m_raspunsuriTotale++;
-}
-
-void User::updateMeciuriJucate()
-{
-	m_meciuriJucate++;
-}
-
-void User::updateScorMinim(int scor)
-{
-	if (scor < m_scorMinim)
-	{
-		m_scorMinim = scor;
-	}
-}
-
-void User::updateScorMaxim(int scor)
-{
-	if (scor > m_scorMaxim)
-	{
-		m_scorMaxim = scor;
-	}
-}
-
 
 std::istream& operator>>(std::istream& in, User& user)
 {
 	in >> user.user_name >> user.password;
-	in>>user.m_raspunsIntrebariCorecte;
-	in>>user.m_raspunsuriTotale;
-	in>>user.m_meciuriJucate;
-	in>>user.m_scorMinim;
-	in>>user.m_scorMaxim;
 	return in;
 }
 
 std::ostream& operator<<(std::ostream& out, User& user)
 {
-	out << user.user_name << ' ' << user.password<<' '<<user.m_raspunsIntrebariCorecte<<' '<<user.m_raspunsuriTotale<<' '<<user.m_meciuriJucate<<' ';
-	out << user.m_scorMinim << ' ' << user.m_scorMaxim;
-
+	out << user.user_name << ' ' << user.password;
 	return out;
 }
--- Back_end/User.h
+++ Back_end/User.h
@@ -1,42 +1,35 @@
 #pragma once
 #include <fstream>
-#include <iostream>
-
+#include <sqlite_orm/sqlite_orm.h>
+namespace sql = sqlite_orm;
 class User
 {
 private:
 	std::string user_name, password;
-	int m_raspunsIntrebariCorecte;
-	int m_raspunsuriTotale;
-	int m_meciuriJucate;
-	int m_scorMaxim, m_scorMinim;
 
 public:
 	User() = default;
 	User(std::string userName, std::string pass);
-
-	User(std::string userName, std::string pass,int raspunsIntrebariCorecte,int raspunsuriTotale,int meciuriJucate,int scorMaxim,int scorMinim);
-	
 	std::string getUserName() const;
 	std::string getPassword() const;
+	void setUserName(std::string a);
+	void setPassword(std::string a);
 	void changePassword(const std::string& pass);
 	friend std::istream& operator >> (std::istream& in, User& user);
 	friend std::ostream& operator << (std::ostream& out, User& user);
 	std::string checkStrongPassword();
 	void forgotPasswordProtocol(std::ostream& out, std::istream& in);
 	~User() = default;
-
-	float getProcentajRaspunsuriCorecte();
-	int getMeciuriJucate();
-	int getScorMinim();
-	int getScorMaxim();
-	int getRaspunsuriCorecte();
-	int getRaspunsuriTotale();
-
-	void updateRaspunsuriCorecte();
-	void updateRaspunsuriTotale();
-	void updateMeciuriJucate();
-	void updateScorMinim(int scor);
-	void updateScorMaxim(int scor);
 };
 
+inline auto createStorage(const std::string& filename)
+{
+	return sql::make_storage(
+		filename,
+		sql::make_table(
+			"Products",
+			sql::make_column("USER_NAME", &User::getUserName,&User::setUserName, sql::autoincrement(), sql::primary_key()),
+			sql::make_column("PASSWORD", &User::getPassword,&User::setPassword)
+		)
+	);
+}
