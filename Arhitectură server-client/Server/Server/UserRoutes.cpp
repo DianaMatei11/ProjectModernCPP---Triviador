25d19232aa583490aa0c9dff0687c32d55ca60e2
Ionel14 <vulacaneanionel@gmail.com>
2022-12-11 19:23:18 +0200

    Am adaugat functorul necesar pt adaugarea unui nou user in baza de date

--- Arhitectură server-client/Server/Server/UserRoutes.cpp
+++ Arhitectură server-client/Server/Server/UserRoutes.cpp
@@ -18,10 +18,40 @@ void routeForSignin(crow::SimpleApp& app, Storage& m_db)
 {
 	auto& user = CROW_ROUTE(app, "/createnewuser")
 		.methods(crow::HTTPMethod::PUT);
-	//user(AddNewUserHandler(m_db));
+	user(AddNewUserHandler(m_db));
 }
 
 
+
+AddNewUserHandler::AddNewUserHandler(Storage& storage) :
+	m_db{ storage }
+{}
+
+crow::response AddNewUserHandler::operator() (const crow::request& req) const
+{
+	auto bodyArgs = parseUrlArgs(req.body); //id=2&quantity=3&...
+	auto end = bodyArgs.end();
+	auto nameIter = bodyArgs.find("Name");
+	auto passIter = bodyArgs.find("Password");
+	if (nameIter != end && passIter != end)
+	{
+		if (existUserName(nameIter->second, m_db).has_value())
+		{
+			return crow::response(451); //UserName already exist
+		}
+
+
+		if (User::checkStrongPassword(passIter->second) != "Strong\n")
+		{
+			return crow::response(416); //Password not strong enough
+		}
+
+
+		m_db.insert(User{ nameIter->second, passIter->second });
+		return crow::response(200); //Added to database
+	}
+	return crow::response(400); //The transmited info was not ok 
+}
 
 
 //login
@@ -31,5 +61,3 @@ void routeForLogin(crow::SimpleApp& app, Storage& m_db)
 		.methods(crow::HTTPMethod::PUT);
 	//user(VerifyUserLogin(m_db));
 }
-
-
--- Arhitectură server-client/Server/Server/UserRoutes.h
+++ Arhitectură server-client/Server/Server/UserRoutes.h
@@ -5,5 +5,13 @@
 std::optional<User> existUserName(const std::string& username, Storage& m_db);
 
 void routeForSignin(crow::SimpleApp& app, Storage& m_db);
+class AddNewUserHandler {
+public:
+	AddNewUserHandler(Storage& storage);
+
+	crow::response operator() (const crow::request& req) const;
+private:
+	Storage& m_db;
+};
 
 void routeForLogin(crow::SimpleApp& app, Storage& m_db);
