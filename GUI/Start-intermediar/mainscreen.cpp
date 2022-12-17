3629b4e9a242093c76bad4f4b1bbecccb1dff063
SandorViviana <viviana.sandor@student.unitbv.ro>
2022-12-17 18:07:57 +0200

    Adaugare legaturi intre ecran principal cont si statistici respectiv configurare joc; design interfata statistici de joc

--- GUI/Start-intermediar/mainscreen.cpp
+++ GUI/Start-intermediar/mainscreen.cpp
@@ -6,14 +6,51 @@ MainScreen::MainScreen(QWidget *parent) :
     ui(new Ui::MainScreen)
 {
     ui->setupUi(this);
+   QPixmap bkgnd(":/images/images/AccountScreen.jpg");
+   bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+   QPalette palette;
+   palette.setBrush(QPalette::Window,bkgnd);
+   this->setPalette(palette);
+
+
+    //this->setStyleSheet("background-image:url(:/images/images/AccountScreen.jpg; background-repeat:no-repeat;");
+   
+
+    ui->setupUi(this);
     QPixmap pixmap(":/images/images/logoFinal.png");
     pixmap= pixmap.scaled(this->size(), Qt::IgnoreAspectRatio);
     ui->logo->setPixmap(pixmap);
+    ui->logo2->setPixmap(pixmap);
 
+    QPixmap pixmap2(":/images/images/back_icon.png");
+    pixmap2= pixmap2.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QIcon aux(pixmap2);
+    ui->back->setIcon(aux);
 
+    ui->account->setCurrentIndex(0);
 }
 
 MainScreen::~MainScreen()
 {
     delete ui;
 }
+
+
+
+void MainScreen::on_configuration_clicked()
+{
+    ui->account->setCurrentIndex(2);
+}
+
+
+void MainScreen::on_statistics_clicked()
+{
+    ui->account->setCurrentIndex(1);
+}
+
+
+void MainScreen::on_back_clicked()
+{
+    ui->account->setCurrentIndex(0);
+}
+
--- GUI/Start-intermediar/mainscreen.h
+++ GUI/Start-intermediar/mainscreen.h
@@ -2,6 +2,8 @@
 #define MAINSCREEN_H
 
 #include <QMainWindow>
+//#include <cpr/cpr.h>
+//#include <crow.h>
 
 namespace Ui {
 class MainScreen;
@@ -14,6 +16,13 @@ class MainScreen : public QMainWindow
 public:
     explicit MainScreen(QWidget *parent = nullptr);
     ~MainScreen();
+
+private slots:
+    void on_configuration_clicked();
+
+    void on_statistics_clicked();
+
+    void on_back_clicked();
 
 private:
     Ui::MainScreen *ui;
